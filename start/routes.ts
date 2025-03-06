f573562766bf24ff2768ddd739d5a9b480f7ee44
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-03-06 18:24:00 +0100

    definition de la route getprofil

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,8 @@
 |
 */
 
+import TweetControleursController from '#controllers/tweet_controleurs_controller'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+router.get('username', [TweetControleursController, 'getProfil'])
