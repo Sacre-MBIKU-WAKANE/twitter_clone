0bb5692e24fc418542cda838b4bd0543515b6ce4
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-03-07 23:46:19 +0100

    add methode dans le controleurs

--- start/routes.ts
+++ start/routes.ts
@@ -12,3 +12,4 @@ import router from '@adonisjs/core/services/router'
 const TweetControleursController = () => import('#controllers/tweet_controleurs_controller')
 router.on('/').render('pages/home')
 router.get('username', [TweetControleursController, 'getProfil'])
+router.get('userTweet/:idUser', [TweetControleursController, 'getPostHome'])
