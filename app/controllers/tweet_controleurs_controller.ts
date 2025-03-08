9e340a87384361bb95b261deccd5abc0ad05dbba
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-03-08 19:27:09 +0100

    suppresion de certain code

--- app/controllers/tweet_controleurs_controller.ts
+++ app/controllers/tweet_controleurs_controller.ts
@@ -1,25 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { users, tweets } from '../../resources/data/data.ts'
 
-// type Posts = {
-//   id: number
-//   IDUser: string
-//   contenu: string
-//   img: string
-//   date_pub: Date
-// }
-
-// const posts: Posts[] = [
-//   {
-//     id: 1,
-//     IDUser: 'CNN',
-//     contenu:
-//       'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a major breakthrough that would serve to both strengthen the US steel industry and combat the global climate crisis.',
-//     img: '',
-//     date_pub: new Date(),
-//   },
-// ]
-
 // class controleur tweet
 export default class TweetControleursController {
   // methode permettant d'afficher les tweets sur la page d'accueil
